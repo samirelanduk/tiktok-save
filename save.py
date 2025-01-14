0ad067e6780895c10337c36f98d357b6bacd744b
fraxiinus <iamthebow@pm.me>
2025-01-13 22:38:00 -0500

    my tweaks

--- save.py
+++ save.py
@@ -5,6 +5,7 @@ import json
 import sys
 import random
 import time
+import requests
 from tqdm import tqdm
 from TikTokApi import TikTokApi
 from utilities import *
@@ -58,6 +59,7 @@ async def get_videos():
         activity["Favorite Videos"]["FavoriteVideoList"]
     
         # What videos are already accounted for?
+        print("Checking for videos to download...")
         videos = videos_to_check(videos, location, check_failures)
 
         # Worth doing anything?
@@ -71,10 +73,11 @@ async def get_videos():
         for videoInfo in tqdm(videos):
             author_unique_id = "unknown"  # Default value
             try:
-                timestamp = date_to_timestamp(videoInfo["Date"])
-                video = api.video(url=videoInfo["Link"])
+                video = api.video(url=videoInfo["link"])
                 tiktok_id = video.id[:-1]
-
+                
+                # define variable as empty in case video info fails
+                tiktok_dict = {}
                 video_info = await video.info()
                 tiktok_dict = video_info  # This is already a dictionary
 
@@ -85,25 +88,42 @@ async def get_videos():
                 if keywords:
                     try:
                         if not should_download(tiktok_dict, keywords):
-                            print(f"Skipping video {tiktok_id} as it doesn't match the keywords")
+                            tqdm.write(f"Skipping video {tiktok_id} as it doesn't match the keywords")
                             continue
                     except Exception as e:
-                        print(f"Error filtering video {tiktok_id}: {str(e)}")
+                        tqdm.write(f"Error filtering video {tiktok_id}: {str(e)}")
                         continue
-
-                video_bytes = await video.bytes()
-
-                save_files(location, tiktok_dict, video_bytes, timestamp, tiktok_id)
+                
+                # handle image posts here
+                if "imagePost" in tiktok_dict:
+                    imageBytes = download_images(tiktok_dict["imagePost"]["images"])
+                    save_files(location, tiktok_dict, imageBytes, tiktok_id, "images")
+                else:
+                    video_bytes = await video.bytes()
+                    save_files(location, tiktok_dict, video_bytes, tiktok_id, "video")
                 
                 # Remove from failures if it was successfully downloaded
                 failures.pop(tiktok_id, None)
                 
                 time.sleep(1)  # don't be suspicious
             except Exception as e:
-                error_message = f"Error processing video {videoInfo['Link']}: {str(e)}"
-                print(error_message)
+                error_message = f"Error processing video {videoInfo['link']}: {str(e)}"
+
+                # Retry video download using manual request method + alternative bitrate url
+                if tiktok_dict is not None and "imagePost" not in tiktok_dict:
+                    try:
+                        altVideoBytes = alt_video_download(tiktok_dict)
+                        save_files(location, tiktok_dict, altVideoBytes, tiktok_id, "video_alt")
+
+                        failures.pop(tiktok_id, None)
+                        time.sleep(1)  # don't be suspicious
+                        continue
+                    except Exception as e2:
+                        error_message = f"Failed again for {videoInfo['link']}: {str(e2)}"
+
+                tqdm.write(error_message)
                 failures[tiktok_id] = {
-                    "link": videoInfo["Link"],
+                    "link": videoInfo["link"],
                     "error": str(e),
                     "traceback": traceback.format_exc(),
                     "timestamp": time.time(),
@@ -117,6 +137,52 @@ async def get_videos():
 
         # Save unique IDs with failure counts to a file
         save_unique_ids_with_counts(location, unique_ids_count)
+
+def download_images(images):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'HX-Request': 'true',
+        'HX-Trigger': 'search-btn',
+        'HX-Target': 'tiktok-parse-result',
+        'HX-Current-URL': 'https://tiktokio.com/',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'https://tiktokio.com',
+        'Connection': 'keep-alive',
+        'Referer': 'https://tiktokio.com/'
+    }
+
+    imageData = []
+    with requests.Session() as s:
+        for imageDict in images:
+            imgUrl = imageDict["imageURL"]["urlList"][0]
+            imageData.append(s.get(imgUrl).content)
+
+    return imageData
+
+def alt_video_download(tiktok_dict):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'HX-Request': 'true',
+        'HX-Trigger': 'search-btn',
+        'HX-Target': 'tiktok-parse-result',
+        'HX-Current-URL': 'https://tiktokio.com/',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'https://tiktokio.com',
+        'Connection': 'keep-alive',
+        'Referer': 'https://tiktokio.com/'
+    }
+
+    altVideoUrls = tiktok_dict["video"]["bitrateInfo"][0]["PlayAddr"]["UrlList"]
+    for url in altVideoUrls:
+        if url.startswith("https://www.tiktok.com"):
+            with requests.Session() as s:
+                videoStream = s.get(url, stream=True)
+
+    return videoStream
 
 def should_download(video_info, keywords):
     """Check if the video matches any of the given keywords."""
